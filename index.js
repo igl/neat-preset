0037971f30426ecd9c22359d96351cf48e7fd35c
Richard Keil <formenwandler@gmail.com>
2019-03-15 17:30:35 +0100

    fix: add patch for shitty 'graphql-language-service-interface'

--- index.js
+++ index.js
@@ -5,8 +5,34 @@ const withImages = require('next-images')
 const withFonts = require('next-fonts')
 const withCSS = require('@zeit/next-css')
 
+const withGraphqlLanguageServiceInterfaceFix = (nextConfig = {}) => ({
+    ...nextConfig,
+    webpack (config, options) {
+        const webpack = require('webpack')
+
+        config.module.rules.unshift(
+            {
+                'test': /\.(flow)$/,
+                'use': 'ignore-loader',
+            }
+        )
+
+        config.plugins.unshift(new webpack.ContextReplacementPlugin(
+            /graphql-language-service-interface[\\/]dist$/,
+            new RegExp(`^\\./.*\\.js$`)
+        ))
+
+        if (typeof nextConfig.webpack === 'function') {
+            return nextConfig.webpack(config, options)
+        }
+
+        return config
+    },
+})
+
 module.exports = (options = {}) => (nextConfig = {}) => {
-    nextConfig = withEnv({ ...nextConfig, ...options.env })
+    nextConfig = withEnv(options.env)(nextConfig)
+    nextConfig = withGraphqlLanguageServiceInterfaceFix(nextConfig)
     nextConfig = withFonts({ ...nextConfig, ...options.fonts })
     nextConfig = withImages({ ...nextConfig, ...options.images })
     nextConfig = withCSS({ ...nextConfig, ...options.css })
