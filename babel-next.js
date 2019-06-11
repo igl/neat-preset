4360f1290efa843edcb93a4de5de61815d5e427a
Richard Keil <igl@formenwandler.com>
2019-06-11 12:05:44 +0200

    chore: add configure module to centralizing config generation

--- babel-next.js
+++ babel-next.js
@@ -1,37 +1,32 @@
 module.exports = (context, options = {}) => {
+    const configure = require('./lib/configure')
 
-    const styledComponentsOptions = {
-        ssr: true,
-        displayName: true, 
-        ...options['styled-components']
-    }
-
-    const presetTypescriptOptions = {
-        isTSX: true,
-        ...options['preset-typescript']
-    }
+    const presetTypescriptOptions = configure.typescript(options, 'next')
+    const styledComponentsOptions = configure.styledComponents(options, 'next')
+    const transformDefineOptions = configure.transformDefine(options, 'next')
+    const optionalChainingOptions = configure.optionalChaining(options, 'next')
+    const moduleResolverOptions = configure.moduleResolver(options, 'next')
 
     return {
         presets: [
             [ require('next/babel'), options ],
-            [ require('@babel/preset-typescript'), presetTypescriptOptions ]
+            [ require('@babel/preset-typescript'), presetTypescriptOptions ],
         ],
         plugins: [
-            require('babel-plugin-jsx-control-statements'),
-            ...require('./lib/babel-plugins')(options)
-        ],
-        env: {
-            development: {
-                plugins: [
-                    [ require('babel-plugin-styled-components'), { minify: false, ...styledComponentsOptions } ]
-                ]
-            },
-            production: {
-                plugins: [
-                    [ require('babel-plugin-styled-components'), { minify: true, ...styledComponentsOptions } ]
-                ]
-            }
-        }
-    }
+            // frontend only
+            [ require('babel-plugin-jsx-control-statements') ],
+            [ require('babel-plugin-styled-components'), styledComponentsOptions ],
 
+            // additional babel plugins
+            [ require('babel-plugin-macros') ],
+            [ require('@babel/plugin-proposal-export-default-from') ],
+            [ require('@babel/plugin-proposal-export-namespace-from') ],
+            [ require('@babel/plugin-proposal-optional-catch-binding') ],
+            [ require('@babel/plugin-proposal-throw-expressions') ],
+            [ require('@babel/plugin-proposal-partial-application') ],
+            [ require('babel-plugin-transform-define'), transformDefineOptions ],
+            [ require('@babel/plugin-proposal-optional-chaining'), optionalChainingOptions ],
+            [ require('babel-plugin-module-resolver'), moduleResolverOptions ],
+        ]
+    }
 }
