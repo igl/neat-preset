51f8bd00fcd67b1fb224963da2417beb9f87c837
Richard Keil <igl@formenwandler.com>
2019-06-12 10:35:06 +0200

    chore(release): next-dev v0.0.3

--- pkgs/next-dev/index.js
+++ pkgs/next-dev/index.js
@@ -1,48 +1,62 @@
-const withNeat = require('@neat-preset/next')
+const { PHASE_PRODUCTION_SERVER = null } =
+    process.env.NODE_ENV === 'development'
+        ? {} // We're never in "production server" phase when in development mode
+        : require('@neat-preset/next/constants')
 
-module.exports = (neatOptions = {}) =>
-    withNeat(neatOptions)((phase, { defaultConfig }) => {
-        const withImages = require('next-images')
-        const withFonts = require('next-fonts')
-        const withCSS = require('@zeit/next-css')
-        const withTypescript = require('@zeit/next-typescript')
+module.exports = (neatOptions = {}) => {
+    const withNeat = require('@neat-preset/next')(neatOptions)
 
-        const withGraphqlLanguageServiceInterfaceFix = (nextConfig = {}) => ({
-            ...nextConfig,
-            webpack(config, options) {
-                const webpack = require('webpack')
+    return (nextConfig = {}, composePlugins = {}) => {
+        const { nextComposePlugins, phase } = composePlugins
 
-                config.module.rules.unshift({
-                    test: /\.(flow)$/,
-                    use: 'ignore-loader',
-                })
+        const nextConfigMethod = (phase, args) => {
+            if (typeof nextConfig === 'function') {
+                nextConfig = nextConfig(phase, args)
+            }
 
-                config.plugins.unshift(
-                    new webpack.ContextReplacementPlugin(
-                        /graphql-language-service-interface[\\/]dist$/,
-                        new RegExp(`^\\./.*\\.js$`)
+            if (phase === PHASE_PRODUCTION_SERVER) {
+                return nextConfig
+            }
+
+            const withImages = require('next-images')
+            const withFonts = require('next-fonts')
+            const withCSS = require('@zeit/next-css')
+            const withTypescript = require('@zeit/next-typescript')
+
+            const withGraphqlLanguageServiceInterfaceFix = (nextConfig = {}) => ({
+                ...nextConfig,
+                webpack(config, options) {
+                    const webpack = require('webpack')
+
+                    config.module.rules.unshift({
+                        test: /\.(flow)$/,
+                        use: 'ignore-loader',
+                    })
+
+                    config.plugins.unshift(
+                        new webpack.ContextReplacementPlugin(
+                            /graphql-language-service-interface[\\/]dist$/,
+                            new RegExp(`^\\./.*\\.js$`)
+                        )
                     )
-                )
 
-                if (typeof nextConfig.webpack === 'function') {
-                    return nextConfig.webpack(config, options)
-                }
+                    if (typeof nextConfig.webpack === 'function') {
+                        return nextConfig.webpack(config, options)
+                    }
 
-                return config
-            },
-        })
+                    return config
+                },
+            })
 
-        return (nextConfig = {}) => (phase, args) => {
             nextConfig = withGraphqlLanguageServiceInterfaceFix(nextConfig)
             nextConfig = withFonts({ ...nextConfig, ...neatOptions.fonts })
             nextConfig = withImages({ ...nextConfig, ...neatOptions.images })
             nextConfig = withCSS({ ...nextConfig, ...neatOptions.css })
             nextConfig = withTypescript({ ...nextConfig, ...neatOptions.typescript })
 
-            if (typeof nextConfig === 'function') {
-                return nextConfig(phase, args)
-            }
-
             return nextConfig
         }
-    })
+
+        return withNeat(nextComposePlugins ? nextConfigMethod(phase) : nextConfigMethod)
+    }
+}
