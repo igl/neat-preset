7206c2fac951c6e0ff0c7301ff820c005c567e3d
Richard Keil <igl@formenwandler.com>
2020-01-14 13:14:56 +0100

    chore: added bump task

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 100,
+    semi: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+}
