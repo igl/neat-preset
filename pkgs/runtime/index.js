0cab94f84b0bb8bf7180ea17e7c2ed49ff75f8a4
Richard Keil <igl@formenwandler.com>
2020-12-06 12:35:19 +0100

    feat(runtime): add tiny-invariant and tiny-warning

--- pkgs/runtime/index.js
+++ pkgs/runtime/index.js
@@ -1,1 +1,10 @@
-/** noop */
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.warning = exports.invariant = void 0;
+
+exports.invariant = require('tiny-invariant');
+exports.warning = require('tiny-warning');
